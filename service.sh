cd83859d8ccd3f493ee15d6b3132947bcabb4ae2
bh public <bh.public@comcast.net>
2017-04-04 14:52:34 -0700

    Update service.sh

--- service.sh
+++ service.sh
@@ -2,22 +2,20 @@
 # Swiftstack service
 
 
-#if [ -n "$gitTag" ]; then
-#    agentSendLogMessage  "Found gitTag parameter gitTag = ${gitTag}"
-#else
-#     agentSendLogMessage  "Didn't find custom parameter gitTag. Using gitTag=master"
-#     gitTag="swiftstack container create"
-#fi
-
+if [ -n "$gitTag" ]; then
+    agentSendLogMessage  "Found gitTag parameter gitTag = ${gitTag}"
+else
+     agentSendLogMessage  "Didn't find custom parameter gitTag. Using gitTag=master"
+     gitTag="swiftstack container create"
+fi
 
 yum install -y python-pip
 pip install pip --upgrade
 pip install nsnitro
 yum install -y python-swiftclient
-
+# Get the python code for the service
 echo Getting external service ...
 wget https://raw.githubusercontent.com/bh-public/cc-ss01/master/SS01.py -O SS01.py
-
-chmod +x SS01.py
+# Run the Service
 echo Running Python script ...
-python SS01.py start
+python SS01.py
